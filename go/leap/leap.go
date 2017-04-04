1f876260c23f8215b186ef4210476c0fbac68bee
Arek Jaskólski <arek.jaskolski@gmail.com>
2017-04-04 12:27:02 +0200

    Hello world in elm

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,18 +1,9 @@
 package leap
 
-// import "fmt"
-// import "time"
-// import "strconv"
 import "math"
 
 const testVersion = 3
 
 func IsLeapYear(i int) bool {
-	// t := time.Now()
-	
-	// i, err := strconv.Atoi(fmt.Sprintf(t.Format("2006")))
-    // if err != nil {
-    //     fmt.Println(err)
-    // }
 	return (math.Mod(float64(i), 4) == 0 && math.Mod(float64(i), 100) != 0) || math.Mod(float64(i), 400) == 0
 }
