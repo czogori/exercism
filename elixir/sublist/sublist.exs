7b29e395163cc595e294aa1a935e4c41d05a298e
Arek Jaskólski <arek.jaskolski@gmail.com>
2017-06-10 14:08:48 +0200

    Next one resolved

---
+++ elixir/sublist/sublist.exs
@@ -0,0 +1,25 @@
+defmodule Sublist do
+  @doc """
+  Returns whether the first list is a sublist or a superlist of the second list
+  and if not whether it is equal or unequal to the second list.
+  """
+  def compare(a, b) when a == b,  do: :equal
+  def compare(a, []) when length(a) > 0, do: :superlist
+  def compare([], b) when length(b) > 0, do: :sublist
+  def compare(a, b) when a != b and length(a) >= length(b), do: if contains(b, a), do: :superlist, else: :unequal
+  def compare(a, b) when a != b and length(a) < length(b), do: if contains(a,b), do: :sublist, else: :unequal
+  def contains(_, []), do: false
+  def contains([h|t], b) do
+    index = Enum.find_index(b, fn n -> n === h end)
+    if index != nil do
+      len = length([h|t]) -1
+      if [h|t] == Enum.slice(b, index..(index + len)) do
+        true
+      else
+        contains([h|t], Enum.slice(b, (index+1)..(length(b)-1)))
+      end
+    else
+      false
+    end
+  end
+end
