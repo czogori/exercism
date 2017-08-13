ed50b1b5c4236b0bef38166ca8d600083b263760
Arek Jaskólski <arek.jaskolski@gmail.com>
2017-08-13 21:08:30 +0200

    Leap

---
+++ erlang/leap/src/leap.erl
@@ -0,0 +1,12 @@
+-module(leap).
+
+-export([leap_year/1, test_version/0]).
+
+leap_year(Year) when (Year rem 4 == 0) and (Year rem 100 /= 0) ->
+  true;
+leap_year(Year) when Year rem 400 == 0 ->
+  true;
+leap_year(_) ->
+  false.
+
+test_version() -> 1.
