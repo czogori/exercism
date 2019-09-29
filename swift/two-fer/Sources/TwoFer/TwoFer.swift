92c6b2fd90c7fdbde166cde1495846061b9369c5
Arek Jaskólski <arek.jaskolski@gmail.com>
2019-09-29 20:00:22 +0200

    TwoFer

---
+++ swift/two-fer/Sources/TwoFer/TwoFer.swift
@@ -0,0 +1,10 @@
+//Solution goes in Sources
+class TwoFer {
+    static func twoFer() -> String {
+        return "One for you, one for me."
+    }
+    
+    static func twoFer(name:String) -> String {
+        return "One for \(name), one for me." 
+    }
+}
