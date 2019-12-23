60f596bb4c9ccad340acc472954229af25a7287c
Arek Jaskólski <arek.jaskolski@gmail.com>
2019-12-23 21:17:59 +0100

    Atbash cipher

---
+++ elixir/atbash-cipher/lib/atbash.ex
@@ -0,0 +1,44 @@
+defmodule Atbash do
+  @plain  ~w(a b c d e f g h i j k l m n o p q r s t u v w x y z 1 2 3 4 5 6 7 8 9)
+  @cipher ~w(z y x w v u t s r q p o n m l k j i h g f e d c b a 1 2 3 4 5 6 7 8 9)
+
+  @doc """
+  Encode a given plaintext to the corresponding ciphertext
+
+  ## Examples
+
+  iex> Atbash.encode("completely insecure")
+  "xlnko vgvob rmhvx fiv"
+  """
+  @spec encode(String.t()) :: String.t()
+  def encode(plaintext) do
+    plaintext
+    |> String.downcase()
+    |> String.replace(~r/[^a-z1-9]/, "")
+    |> String.codepoints()
+    |> Enum.map(&map_letter/1)
+    |> group()
+    |> Enum.join()
+    |> String.trim()
+  end
+
+  @spec decode(String.t()) :: String.t()
+  def decode(cipher) do
+    cipher
+    |> String.replace(" ", "")
+    |> String.codepoints()
+    |> Enum.map(&map_letter/1)
+    |> Enum.join()
+  end
+
+  def map_letter(letter) do
+    Enum.zip(@plain, @cipher)
+    |> Enum.into(%{})
+    |> Map.get(letter)
+  end
+
+  def group(letters, count \\ 1)
+  def group([], _), do: []
+  def group([h|t], count) when rem(count, 5) == 0, do: [h <> " " | group(t, count + 1)]
+  def group([h|t], count), do: [h | group(t, count + 1)]
+end
