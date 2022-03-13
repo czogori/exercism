c3cbb00cc6f87e11c864c8065415b29bb1c08db9
Arek Jaskólski <arek.jaskolski@gmail.com>
2022-03-13 12:57:50 +0100

    Fixed without Enum module

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -6,14 +6,17 @@ defmodule BirdCount do
   def increment_day_count([]), do: [1]
   def increment_day_count([today | rest]), do: [today + 1 | rest]
 
-  def has_day_without_birds?(list), do: Enum.member?(list, 0)
+  def has_day_without_birds?([]), do: false
+  def has_day_without_birds?([0|_]), do: true
+  def has_day_without_birds?([_|rest]), do: has_day_without_birds?(rest)
 
-  def total(list), do: Enum.sum(list)
+  def total(list, sum \\ 0)
+  def total([], sum), do: sum
+  def total([today | rest], sum), do: total(rest, sum + today)
 
   @number_birds_in_busy_day 5
-  def busy_days(list) do
-    list
-    |> Enum.filter(& &1 >= @number_birds_in_busy_day)
-    |> Enum.count()
-  end
+  def busy_days(list, count \\ 0)
+  def busy_days([], count), do: count
+  def busy_days([today | rest], count) when today >= @number_birds_in_busy_day, do: busy_days(rest, count + 1)
+  def busy_days([_ | rest], count), do: busy_days(rest, count)
 end
