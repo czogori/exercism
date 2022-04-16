73b0b84d60f5e3925afd9eeb488303c8994f80bd
Arek Jaskólski <arek.jaskolski@gmail.com>
2022-04-16 16:55:03 +0200

    Delete debug dir

--- rust/lucians-luscious-lasagna/target/debug/deps/lucians_luscious_lasagna-9e8a5c2427a1b08f.d
+++
@@ -1,7 +0,0 @@
-/home/czogori/exercism/rust/lucians-luscious-lasagna/target/debug/deps/lucians_luscious_lasagna-9e8a5c2427a1b08f.rmeta: src/lib.rs
-
-/home/czogori/exercism/rust/lucians-luscious-lasagna/target/debug/deps/liblucians_luscious_lasagna-9e8a5c2427a1b08f.rlib: src/lib.rs
-
-/home/czogori/exercism/rust/lucians-luscious-lasagna/target/debug/deps/lucians_luscious_lasagna-9e8a5c2427a1b08f.d: src/lib.rs
-
-src/lib.rs:
--- rust/lucians-luscious-lasagna/target/debug/deps/lucians_luscious_lasagna-ad854d57b339f86b.d
+++
@@ -1,5 +0,0 @@
-/home/czogori/exercism/rust/lucians-luscious-lasagna/target/debug/deps/lucians_luscious_lasagna-ad854d57b339f86b: src/lib.rs
-
-/home/czogori/exercism/rust/lucians-luscious-lasagna/target/debug/deps/lucians_luscious_lasagna-ad854d57b339f86b.d: src/lib.rs
-
-src/lib.rs:
