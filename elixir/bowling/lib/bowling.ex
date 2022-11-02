1231cc7e2437fa97443972e2f76ed14fcf7bb58b
Arek Jaskólski <arek.jaskolski@gmail.com>
2022-11-02 18:55:08 +0100

    Naming improvments

--- elixir/bowling/lib/bowling.ex
+++ elixir/bowling/lib/bowling.ex
@@ -32,7 +32,7 @@ defmodule Bowling do
     cond do
       length(frames) > 10 && Tuple.sum(frame10) < 10 ->
         {:error, "Cannot roll after game is over"}
-      tuple_size(last) == 2 && pin_count_exceeds(last) ->
+      tuple_size(last) == 2 && pin_count_exceeds?(last) ->
         {:error, "Pin count exceeds pins on the lane"}
       (Enum.count(frames) == 11 && spare?(frame10) && tuple_size(last) > 1) ||
       (Enum.count(frames) == 12 && strike?(frame10) && Tuple.sum(Enum.at(frames, 10)) < 10) ->
@@ -42,7 +42,7 @@ defmodule Bowling do
     end
   end
 
-  defp pin_count_exceeds(rolls) when is_tuple(rolls) do
+  defp pin_count_exceeds?(rolls) when is_tuple(rolls) do
     Tuple.sum(rolls) > @max_point_in_frame
   end
 
@@ -66,44 +66,33 @@ defmodule Bowling do
         score = rolls |> calculate_score()
         {:ok, score}
     end
-
   end
 
   defp calculate_score([]), do: 0
-  defp calculate_score([h1, h2, h3 | t] = rolls) do
+  defp calculate_score([roll1, roll2, roll3 | t] = rolls) do
     cond do
-      h1 == @max_point_in_frame ->
-        strike(rolls)
-      h1 + h2 == @max_point_in_frame ->
-        spare(rolls)
+      roll1 == @max_point_in_frame ->
+        foo(rolls, [roll2, roll3])
+      roll1 + roll2 == @max_point_in_frame ->
+        foo(rolls, [roll3])
       true ->
-        h1 + h2 + calculate_score([h3] ++ t)
+        roll1 + roll2 + calculate_score([roll3] ++ t)
     end
   end
-  defp calculate_score([h1, h2 | t]) do
-    h1 + h2 + calculate_score(t)
+  defp calculate_score([roll1, roll2 | t]) do
+    roll1 + roll2 + calculate_score(t)
   end
 
-  defp spare([h1, h2, h3 | t]) do
-    if Enum.count(t) > 0 do
-      h1 + h2 + h3 + calculate_score([h3] ++ t)
-    else
-      h1 + h2 + h3 + calculate_score(t)
-    end
+  defp foo([roll1, roll2, roll3 | t], add) do
+    roll1 + roll2 + roll3 + if Enum.count(t) > 0,
+    do: calculate_score(add ++ t),
+    else: calculate_score(t)
   end
 
-  defp strike([h1, h2, h3 | t]) do
-    if Enum.count(t) > 0 do
-      h1 + h2 + h3 + calculate_score([h2, h3] ++ t)
-    else
-      h1 + h2 + h3 + calculate_score(t)
-    end
-  end
-
-  defp strike?({roll}) when roll == 10, do: true
+  defp strike?({roll}) when roll == @max_point_in_frame, do: true
   defp strike?(_), do: false
 
-  defp spare?({roll1, roll2}) when roll1 + roll2 == 10, do: true
+  defp spare?({roll1, roll2}) when roll1 + roll2 == @max_point_in_frame, do: true
   defp spare?(_), do: false
 
   defp strike_or_spare?(rolls) when is_tuple(rolls), do:
@@ -114,6 +103,7 @@ defmodule Bowling do
   defp make_frames([]), do: []
   defp make_frames([@max_point_in_frame|t]),
     do: [{@max_point_in_frame}] ++ make_frames(t)
-  defp make_frames([h1, h2|t]), do: [{h1, h2}] ++ make_frames(t)
-  defp make_frames([h|t]), do: [{h}] ++ make_frames(t)
+  defp make_frames([roll1, roll2 | t]),
+    do: [{roll1, roll2}] ++ make_frames(t)
+  defp make_frames([roll|t]), do: [{roll}] ++ make_frames(t)
 end
